04947523d52151bdbeadbb2271b8c4763babb82f
Paul Lam <paul@quantisan.com>
2014-08-10 15:34:06 -0400

    fixes #8

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -7,9 +7,9 @@ if [ -z "$(ls -A /var/lib/mysql)" -a "${1%_safe}" = 'mysqld' ]; then
 		echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
 		exit 1
 	fi
-	
+
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-	
+
 	# These statements _must_ be on individual lines, and _must_ end with
 	# semicolons (no line breaks or comments are permitted).
 	# TODO proper SQL escaping on ALL the things D:
@@ -19,22 +19,22 @@ if [ -z "$(ls -A /var/lib/mysql)" -a "${1%_safe}" = 'mysqld' ]; then
 		DELETE FROM mysql.user WHERE user != "root" OR host != "%" ;
 		DROP DATABASE IF EXISTS test ;
 	EOSQL
-	
+
 	if [ "$MYSQL_DATABASE" ]; then
-		echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" >> "$TEMP_FILE"
+		echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" >> $TEMP_FILE
 	fi
-	
+
 	if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$TEMP_FILE"
-		
+		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> $TEMP_FILE
+
 		if [ "$MYSQL_DATABASE" ]; then
-			echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> "$TEMP_FILE"
+			echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> $TEMP_FILE
 		fi
 	fi
-	
-	echo 'FLUSH PRIVILEGES ;' >> "$TEMP_FILE"
-	
-	set -- "$@" --init-file="$TEMP_FILE"
+
+	echo 'FLUSH PRIVILEGES ;' >> $TEMP_FILE
+
+	set -- "$@" --init-file=$TEMP_FILE
 fi
 
 chown -R mysql:mysql /var/lib/mysql
