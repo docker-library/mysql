bc3e14ea465861af000d0b124a851ebc00772f7d
Lars Tangvald <lars.tangvald@oracle.com>
2015-04-08 07:42:19 +0200

    Add change from #1 to other entrypoint scripts

--- 5.5/docker-entrypoint.sh
+++ 5.5/docker-entrypoint.sh
@@ -52,6 +52,8 @@ if [ "$1" = 'mysqld' ]; then
 
 		tempSqlFile=$(mktemp /tmp/mysql-first-time.XXXXXX.sql)
 		cat > "$tempSqlFile" <<-EOSQL
+			-- What's done in this file shouldn't be replicated
+			SET @@SESSION.SQL_LOG_BIN=0;
 			DELETE FROM mysql.user ;
 			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
