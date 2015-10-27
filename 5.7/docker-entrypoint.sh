07fbe135af656d0ef325e14b964b38f0b3a4491c
Lars Tangvald <lars.tangvald@oracle.com>
2015-10-27 09:01:14 +0100

    Added workaround for MySQL Bug #78957
     Adds --log-bin-index option to --verbose --help command to prevent errors when binary logging is enabled

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$1" = 'mysqld' ]; then
 	# Get config
-	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+	DATADIR="$("$@" --verbose --help --log-bin-index=/tmp/tmp.index 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
 
 	if [ ! -d "$DATADIR/mysql" ]; then
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
