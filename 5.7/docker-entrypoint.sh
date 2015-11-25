c22ce303062435b0483a8ccc9c66095c22a57a7a
Lars Tangvald <lars.tangvald@oracle.com>
2015-11-25 09:16:41 +0100

    Bug#79374	user `mysql.sys` is removed from docker deployment
    The user is needed for the sys schema. Changed script to preserve it.

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -50,7 +50,7 @@ if [ "$1" = 'mysqld' ]; then
 			-- What's done in this file shouldn't be replicated
 			--  or products like mysql-fabric won't work
 			SET @@SESSION.SQL_LOG_BIN=0;
-			DELETE FROM mysql.user ;
+			DELETE FROM mysql.user where user != 'mysql.sys';
 			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 			DROP DATABASE IF EXISTS test ;
