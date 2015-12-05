cf1bf4ff39736ebd78b6504007c470fd37616f4c
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-12-05 22:47:12 +0300

    support for sql.gz dump restore

--- 5.5/docker-entrypoint.sh
+++ 5.5/docker-entrypoint.sh
@@ -83,9 +83,10 @@ if [ "$1" = 'mysqld' ]; then
 		echo
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
-				*.sh)  echo "$0: running $f"; . "$f" ;;
-				*.sql) echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
-				*)     echo "$0: ignoring $f" ;;
+				*.sh)     echo "$0: running $f"; . "$f" ;;
+				*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
+				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}" && echo ;;
+				*)        echo "$0: ignoring $f" ;;
 			esac
 			echo
 		done
