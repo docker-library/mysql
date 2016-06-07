9108dcf96334d7535dd5f7db742e1765f4d29e90
Giuseppe Maxia <g.maxia@gmail.com>
2016-06-07 09:22:24 +0200

    Fix for bug#81723
     Fixing [Bug#81723](https://bugs.mysql.com/bug.php?id=81723): Installing a container with X-Plugin enabled results with an unresponsive server.

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -54,7 +54,7 @@ if [ "$1" = 'mysqld' ]; then
 			-- What's done in this file shouldn't be replicated
 			--  or products like mysql-fabric won't work
 			SET @@SESSION.SQL_LOG_BIN=0;
-			DELETE FROM mysql.user where user != 'mysql.sys';
+			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys');
 			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 			DROP DATABASE IF EXISTS test ;
