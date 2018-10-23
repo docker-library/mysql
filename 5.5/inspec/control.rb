21860a28d6d30b2c980bea6b0d8c8b802ce14202
MySQL Build Team <mysql-build@oss.oracle.com>
2018-10-23 07:01:51 +0000

    Release version 1.1.8
      * Fix script syntax
      * Update build and test scripts
      * Use yum repos to install packages
      * Iterate over correct version variable
      * Bump shell version to 8.0.13
      * Bump server version to 8.0.13
      * Bump 5.5 server version to 5.5.62
      * Bump image version and server 5.6.42 and 5.7.24

---
+++ 5.5/inspec/control.rb
@@ -0,0 +1,17 @@
+control 'container' do
+  impact 0.5
+  describe docker_container('mysql-server') do
+    it { should exist }
+    it { should be_running }
+    its('repo') { should eq 'mysql/mysql-server' }
+    its('ports') { should eq '3306/tcp' }
+    its('command') { should match '/entrypoint.sh mysqld' }
+  end
+end
+control 'server-package' do
+  impact 0.5
+  describe package('mysql-community-server-minimal') do
+    it { should be_installed }
+    its ('version') { should match '5.5.62.*' }
+  end
+end
