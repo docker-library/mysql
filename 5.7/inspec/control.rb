3ba0fde80558857ba8dd877d3297701f48bc85ba
MySQL Build Team <mysql-build@oss.oracle.com>
2019-07-22 15:42:30 +0000

    Release version 1.1.12
      * Bump shell version to 8.0.17
      * Bump versions for 1.1.12 release
      * Release version 1.1.11
      * Bump shell version to 8.0.16
      * wl12360: Fix escaping of special characters in replacement string
      * wl12360: Use new --validate-config option for 8.0
      * Remove unneeded flush privilege command
      * Bump versions for 1.1.11 release

--- 5.7/inspec/control.rb
+++ 5.7/inspec/control.rb
@@ -12,13 +12,13 @@ control 'server-package' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '5.7.26.*' }
+    its ('version') { should match '5.7.27.*' }
   end
 end
 control 'shell-package' do
   impact 0.5
   describe package('mysql-shell') do
     it { should be_installed }
-    its ('version') { should match '8.0.16.*' }
+    its ('version') { should match '8.0.17.*' }
   end
 end
