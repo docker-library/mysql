98318914f0a88b2323d0da48e4e73df63858f4c7
MySQL Build Team <mysql-build@oss.oracle.com>
2019-10-14 14:17:51 +0000

    Release version 1.1.13
      * Bump shell version
      * Bump image version
      * Bump server versions

--- 8.0/docker-entrypoint.sh
+++ 8.0/docker-entrypoint.sh
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 set -e
 
-echo "[Entrypoint] MySQL Docker Image 8.0.17-1.1.12"
+echo "[Entrypoint] MySQL Docker Image 8.0.18-1.1.13"
 # Fetch value from server config
 # We use mysqld --verbose --help instead of my_print_defaults because the
 # latter only show values present in config files, and not server defaults
@@ -201,7 +201,7 @@ password=healthcheckpass
 EOF
 	touch /mysql-init-complete
 	chown -R mysql:mysql "$DATADIR"
-	echo "[Entrypoint] Starting MySQL 8.0.17-1.1.12"
+	echo "[Entrypoint] Starting MySQL 8.0.18-1.1.13"
 fi
 
 exec "$@"
