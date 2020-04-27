26296a305c53a5ec21698d9273d2048995ab2c13
MySQL Build Team <mysql-build@oss.oracle.com>
2020-04-27 05:05:49 +0000

    Release version 1.1.16
      * Bumped versions for release

--- 5.7/inspec/control.rb
+++ 5.7/inspec/control.rb
@@ -12,13 +12,13 @@ control 'server-package' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '5.7.29.*' }
+    its ('version') { should match '5.7.30.*' }
   end
 end
 control 'shell-package' do
   impact 0.5
   describe package('mysql-shell') do
     it { should be_installed }
-    its ('version') { should match '8.0.19.*' }
+    its ('version') { should match '8.0.20.*' }
   end
 end
