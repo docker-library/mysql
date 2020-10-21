1a25a09159a3d7b6b5b8ad0e6c7eb53504b3aab5
Joe Ferguson <joe@infosiftr.com>
2020-10-21 14:00:47 -0700

    no flush privs needed after grant/create user

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -286,8 +286,6 @@ docker_setup_db() {
 			mysql_note "Giving user ${MYSQL_USER} access to schema ${MYSQL_DATABASE}"
 			docker_process_sql --database=mysql <<<"GRANT ALL ON \`${MYSQL_DATABASE//_/\\_}\`.* TO '$MYSQL_USER'@'%' ;"
 		fi
-
-		docker_process_sql --database=mysql <<<"FLUSH PRIVILEGES ;"
 	fi
 }
 
