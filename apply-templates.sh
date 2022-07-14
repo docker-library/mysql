653507234ff9d3d1c0bef2f6093fbae30a7a021d
Tianon Gravi <admwiggin@gmail.com>
2022-07-13 17:13:58 -0700

    Update jq-template for speed improvements

--- apply-templates.sh
+++ apply-templates.sh
@@ -9,7 +9,8 @@ jqt='.jq-template.awk'
 if [ -n "${BASHBREW_SCRIPTS:-}" ]; then
 	jqt="$BASHBREW_SCRIPTS/jq-template.awk"
 elif [ "$BASH_SOURCE" -nt "$jqt" ]; then
-	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/5f0c26381fb7cc78b2d217d58007800bdcfbcfa1/scripts/jq-template.awk'
+	# https://github.com/docker-library/bashbrew/blob/master/scripts/jq-template.awk
+	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/9f6a35772ac863a0241f147c820354e4008edf38/scripts/jq-template.awk'
 fi
 
 if [ "$#" -eq 0 ]; then
