9fbbc18ee6b5efaca983cf609eb686f0a1211178
Tianon Gravi <admwiggin@gmail.com>
2023-07-03 11:09:15 -0700

    Add "suite aliases" ("-oraclelinux8", "-bullseye", etc)

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -79,17 +79,33 @@ for version; do
 	)
 
 	for variant in oracle debian; do
+		export variant
+
 		df="Dockerfile.$variant"
 		commit="$(dirCommit "$version" "$df")"
 
 		variantAliases=( "${versionAliases[@]/%/-$variant}" )
 		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		case "$variant" in
+			debian)
+				suite="$(jq -r '.[env.version][env.variant].suite' versions.json)"
+				variantAliases=( "${versionAliases[@]/%/-$suite}" "${variantAliases[@]}" )
+				variantAliases=( "${variantAliases[@]//latest-/}" )
+				;;
+
+			oracle)
+				ol="$(jq -r '.[env.version][env.variant].variant | split("-")[0]' versions.json)"
+				variantAliases=( "${versionAliases[@]/%/-oraclelinux$ol}" "${variantAliases[@]}" )
+				variantAliases=( "${variantAliases[@]//latest-/}" )
+				;;
+		esac
+
 		if [ "$variant" = "$defaultVariant" ]; then
 			variantAliases=( "${versionAliases[@]}" "${variantAliases[@]}" )
 		fi
 
 		# TODO if the list of architectures supported by MySQL ever is greater than that of the base image it's FROM, this list will need to be filtered
-		export variant
 		variantArches="$(jq -r '.[env.version][env.variant].architectures | join(", ")' versions.json)"
 
 		echo
